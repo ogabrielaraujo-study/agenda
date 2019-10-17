5637ff59c43a03b781ca715efad3c3e2c667b604
Gabriel Araujo <gabriiel66@gmail.com>
2019-10-17 20:28:23 -0300

    deploy

--- adonis/server.js
+++ adonis/server.js
@@ -1,25 +1,23 @@
-'use strict'
-
-/*
-|--------------------------------------------------------------------------
-| Http server
-|--------------------------------------------------------------------------
-|
-| This file bootstraps Adonisjs to start the HTTP server. You are free to
-| customize the process of booting the http server.
-|
-| """ Loading ace commands """
-|     At times you may want to load ace commands when starting the HTTP server.
-|     Same can be done by chaining `loadCommands()` method after
-|
-| """ Preloading files """
-|     Also you can preload files by calling `preLoad('path/to/file')` method.
-|     Make sure to pass a relative path from the project root.
-*/
-
 const { Ignitor } = require('@adonisjs/ignitor')
+const https = require('https')
+const fs = require('fs')
 
-new Ignitor(require('@adonisjs/fold'))
-  .appRoot(__dirname)
-  .fireHttpServer()
-  .catch(console.error)
+// Certificate
+if (process.env.NODE_ENV == 'production') {
+  const options = {
+    key: fs.readFileSync(process.env.HTTPS_KEY),
+    cert: fs.readFileSync(process.env.HTTPS_CERT),
+  }
+
+  new Ignitor(require('@adonisjs/fold'))
+    .appRoot(__dirname)
+    .fireHttpServer(handler => {
+      return https.createServer(options, handler)
+    })
+    .catch(console.error)
+} else {
+  new Ignitor(require('@adonisjs/fold'))
+    .appRoot(__dirname)
+    .fireHttpServer()
+    .catch(console.error)
+}
