7e3a91a2074ab7b5bd352577b9a74a1d730c7abd
Gabriel Araujo <gabriiel66@gmail.com>
2020-05-09 14:21:07 -0300

    package

--- api/config/database.js
+++ api/config/database.js
@@ -65,18 +65,6 @@ module.exports = {
     debug: Env.get('DB_DEBUG', false),
   },
 
-  cleardb: {
-    client: 'mysql',
-    connection: {
-      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1)),
-    },
-    debug: Env.get('DB_DEBUG', false),
-  },
-
   /*
   |--------------------------------------------------------------------------
   | PostgreSQL
